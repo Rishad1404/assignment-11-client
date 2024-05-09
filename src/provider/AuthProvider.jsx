88f254711a1f546fb8ed69a7d2e0a7e17ee07515
Rishad1404 <rishadislam2019@gmail.com>
2024-05-09 23:49:24 +0600

    first commit

---
+++ src/provider/AuthProvider.jsx
@@ -0,0 +1,96 @@
+import { createContext, useEffect, useState } from "react";
+import PropTypes from 'prop-types';
+import { GoogleAuthProvider, signInWithPopup, GithubAuthProvider, updateProfile } from "firebase/auth";
+import auth from "../firebase/firebase.config";
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+export const AuthContext=createContext(null)
+
+// Social Auth Providers
+const googleProvider = new GoogleAuthProvider();
+const githubProvider = new GithubAuthProvider();
+
+const AuthProvider = ({children}) => {
+const [user,setUser]=useState(null)
+console.log(user)
+const [loading,setLoading]=useState(true)
+
+// Create User
+const createUser=(email,password)=>{
+    setLoading(true)
+    return createUserWithEmailAndPassword(auth,email,password)
+    }
+
+// Update User Profile
+const updateUserProfile=(name,photo)=>{
+    return updateProfile(auth.currentUser, {
+        displayName:name, 
+        photoURL: photo,
+      })
+      
+}
+// Update Email
+const updateEmail = (Email) => {
+    updateEmail(auth.currentUser, {
+        email:Email
+    })
+}
+
+
+    // Sign In user
+const signIn=(email,password)=>{
+    setLoading(true)
+    return signInWithEmailAndPassword(auth,email,password)
+}
+
+
+// Log out user
+const logOut=()=>{
+    setLoading(true)
+    return signOut(auth);
+}
+
+// Google Login
+const googleLogin=()=>{
+    return signInWithPopup(auth,googleProvider)
+}
+// Github Login
+const githubLogin=()=>{
+    return signInWithPopup(auth,githubProvider)
+}
+useEffect(()=>{
+    const unSubscribe= onAuthStateChanged(auth,currentUser=>{
+        setUser(currentUser);
+        setLoading(false)
+    });
+    return()=>{
+        unSubscribe();
+    }
+},[])
+
+const authInfo={
+    user,
+    loading,
+    createUser,
+    signIn,
+    logOut,
+    googleLogin,
+    githubLogin,
+    updateUserProfile,
+    updateEmail
+    
+}
+
+    return (
+        <div>
+            <AuthContext.Provider value={authInfo}>
+                {children}
+            </AuthContext.Provider>
+        </div>
+    );
+};
+
+AuthProvider.propTypes={
+    children:PropTypes.object
+}
+
+export default AuthProvider;
---
+++ src/routes/PrivateRoute.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+import { useContext } from "react";
+import {Navigate,useLocation} from 'react-router-dom'
+import { AuthContext } from '../provider/AuthProvider';
+
+const PrivateRoute = ({children}) => {
+    const {user,loading}=useContext(AuthContext)
+    const location=useLocation();
+    console.log(location)
+
+    if(loading){
+        return <div className="w-16 h-16 lg:mx-[900px] lg:my-[300px] border-4 border-dashed rounded-full animate-spin dark:border-violet-600"></div>
+        }
+
+    if(!user){
+        return <Navigate state={location?.pathname || '/'} to='/login'></Navigate>;
+    }
+    return (
+        <div>
+            {children}
+        </div>
+    )
+};
+PrivateRoute.propTypes={
+    children:PropTypes.object
+}
+
+export default PrivateRoute;
